442e6dcaf5950f22c195e1060f2e3c1188f2b84c
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-09-06 09:54:33 +0100

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -152,7 +152,7 @@ export default function SignupPage() {
 
     try {
       const response = await axios.post(
-        `${process.env.REACT_APP_API_URL}/signup`,
+        'https://snt-frncs-backend-mauve.vercel.app/api/signup', // Replaced process.env.REACT_APP_API_URL
         { email, password }
       );
 
